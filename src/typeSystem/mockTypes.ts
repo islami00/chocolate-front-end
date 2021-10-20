c7e28050130ffb605f652901e1b03758eb0fab05
islami00 <76726548+islami00@users.noreply.github.com>
2021-10-20 18:11:20 +0100

    Added type defs for project and reviews

---
+++ src/typeSystem/mockTypes.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// takes each type as defined in appTypes and exports 8 instances to mocks folder
+
+import { AnyNumber } from '@polkadot/types/types';
+import { ProposalStatus } from './jsonTypes';
+// store on ipfs
+export interface NewMetaData {
+  projectName: string;
+  website: string;
+  whitepaper: string;
+  projectLogo: string;
+}
+// this links to a mapping from id to review
+type ReviewId = AnyNumber;
+
+// The immediate members are stored on-chain
+export interface NewProject {
+  ownerID: string;
+  reviews: ReviewId[];
+  badge: boolean;
+  metaData: NewMetaData;
+  proposalStatus: ProposalStatus;
+}
+
+// store on ipfs fully
+export interface NewReview {
+  proposalStatus: ProposalStatus;
+  userID: string;
+  content: { 
+        reviewText: string;
+        rating: AnyNumber;
+    }
+  projectID: AnyNumber;
+}
