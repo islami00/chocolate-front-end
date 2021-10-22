44ef9c7fde21c54f314520f43123777ee75e339e
Islam <76726548+islami00@users.noreply.github.com>
2021-10-22 06:15:55 +0100

    Added types and json gen

--- src/typeSystem/appTypes.ts
+++ src/typeSystem/appTypes.ts
@@ -1,15 +1,27 @@
-import { Review, User } from './jsonTypes';
+// defined the various classes
+// 
 
-export interface AppInitState {
-  userData?: UserData;
+import { AnyNumber } from '@polkadot/types/types';
+import { NewProject } from './mockTypes';
+
+/**
+ * @description this class decorates a plain object with our ideal project type with useful getter functions
+ * It retains a base project that can be destructured, and also has useful getters for rankPoints and the like that utilise the base struct.
+ */
+class JSONProject {
+  project: NewProject;
+
+  constructor(fresh: NewProject) {
+    this.project = fresh;
+  }
+
+  get rankAverage() {
+    const { reviews } = this.project;
+    const x: AnyNumber = 0;
+    return x;
+  }
+
+ 
 }
 
-export interface AppState {
-  userData: UserData;
-}
-export interface UserData extends User {
-  name: string;
-  accountAddress: string;
-  userReviews: Review[];
-  accountType: '' | 'unset' | 'user' | 'project';
-}
+export default {};
