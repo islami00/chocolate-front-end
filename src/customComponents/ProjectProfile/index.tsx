2d6467c1cb98d4d86e6d82022937b66049d64823
islam-machine <76726548+islami00@users.noreply.github.com>
2021-10-29 08:36:50 +0100

    Added json class to average reviews.

--- src/customComponents/ProjectProfile/index.tsx
+++ src/customComponents/ProjectProfile/index.tsx
@@ -1,5 +1,8 @@
 import { message } from 'chocolate/App';
 import { ProjectAl } from 'chocolate/interfaces';
+import { JSONProject } from 'chocolate/typeSystem/appTypes';
+import { ChainProject } from 'chocolate/typeSystem/jsonTypes';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useApp } from '../state';
 import { filter } from './majorUtils';
@@ -14,14 +17,40 @@ const ProjectProfile: React.FC<{ data: ProjectAl; id: string }> = function (
   const { state } = useApp();
   const { userData } = state;
   const { accountAddress: addr } = userData;
+  const [avRate, setAvRate] = useState('0.0');
   let canReview = true;
   if (data.ownerID.eq(addr)) canReview = false;
   // race!
 
-  const { data: reviews, isLoading: lrev } = useReviews(data, id, addr);
+  const {
+    data: reviews,
+    isLoading: lrev,
+    isFetched: frev,
+  } = useReviews(data, id, addr);
 
-  const { data: projectMeta, isLoading: lprm } = useProjectMeta(data, id);
-  return <h1>Finally, some data fetching, loading? {lrev ? 'Yes' : 'No'} </h1>;
+  const {
+    data: projectMeta,
+    isLoading: lprm,
+    isFetched: fproj,
+  } = useProjectMeta(data, id);
+  // get average ranking
+  useEffect(() => {
+    if (frev && fproj) {
+      const nPr: ChainProject = data.toHuman() as unknown as ChainProject;
+      const useThis = { ...nPr, metaData: projectMeta };
+      const pr = new JSONProject(useThis, reviews);
+      setAvRate(pr.rankAverage.toPrecision(2));
+    }
+    return () => {};
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [frev, fproj]);
+  // structure data here. Button component and the like
+  return (
+    <h1>
+      Finally, some data fetching, loading? {lrev ? 'Yes' : 'No'}, and average?{' '}
+      {avRate}{' '}
+    </h1>
+  );
 };
 const Main: React.FC = function () {
   const { id } = useParams<{ id: string }>();
