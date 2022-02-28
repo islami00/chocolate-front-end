7dd566da61937500f6391d7e91e92aee0ee91e10
Islam <islamkwnd@gmail.com>
2022-02-28 06:14:54 +0000

    Gcloud deployed. awaiting secrets.

---
+++ functions/scripts/init.sh
@@ -0,0 +1,14 @@
+# Init gcloud for dep.
+# Install deps
+sudo apt-get install apt-transport-https ca-certificates gnupg
+# add gcloud pkg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+# cli
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+# init --docker
+# RUN echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && apt-get update -y && apt-get install google-cloud-sdk -y
+# Init
+gcloud init
+# start app.
+# Manual deploy in 3..2..1..
--- functions/src/test.ts
+++ functions/src/test.ts
@@ -1,9 +1,12 @@
+// Enable debugging--gcloud
 // Import app.
-// Serve
+import * as gcdbg from "@google-cloud/debug-agent";
+gcdbg.start({ serviceContext: { enableCanary: false }});
 import * as http from "http";
 import {port} from "./config";
 import {app} from "./httpFx";
 
+// Serve
 http.createServer(app).listen(port);
 // On good call, should server err due to firebase config not being available.
 
