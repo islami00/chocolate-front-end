c33be34c489c6b62c741fd9e8dded03b472727ec
Islam <islamkwnd@gmail.com>
2022-03-06 20:08:22 +0000

    (Chore) comment cleanup

--- src/customComponents/WallOfShame/index.tsx
+++ src/customComponents/WallOfShame/index.tsx
@@ -3,10 +3,9 @@ import { ProjectsView } from '../Projects';
 import { useSearchData } from '../ProjectsRe/hooks';
 
 function WallOfShame(): JSX.Element {
-  // Replace here too.
+
   const { api } = useSubstrate();
   const [data] = useSearchData(api);
-  // Data is never undefined. Stable API.
   return <ProjectsView shame data={data} />;
 }
 
