22e21a161015e419c6b3023c02527fd5d99d3002
Islam <islamkwnd@gmail.com>
2022-05-19 21:46:19 +0000

    build(web-app): :heavy_plus_sign: Added mantine, rm semantic-ui
    Temporary removed semantic-ui styles in favour of mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable import/no-unresolved */
-import 'semantic-ui-css/semantic.min.css';
 import { AppLayout } from './Layouts/app/AppLayout';
 import { AppProvider } from './Layouts/app/AppProvider';
-// styles
-import './styles/index.css';
+// released the brakes on styles
 
 export default function App(): JSX.Element {
   return (
